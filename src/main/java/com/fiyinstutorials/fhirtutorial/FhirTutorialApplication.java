52a0c11fd99f7489e0fab7da6e613372282be581
Fiyinfoluwa Akano <fiyin.akano@upperlink.ng>
2024-06-06 17:14:22 +0100

    FHIR Tutorial (Java)
    This is a basic Java project to interact with an open FHIR server

---
+++ src/main/java/com/fiyinstutorials/fhirtutorial/FhirTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.fiyinstutorials.fhirtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhirTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhirTutorialApplication.class, args);
+	}
+
+}
